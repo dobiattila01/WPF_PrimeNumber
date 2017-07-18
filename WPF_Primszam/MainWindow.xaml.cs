6ab4103487e97cceb4e48d6f5c93161c0c73fd3a
dobiattila01 <dobi.attila@hotmail.com>
2017-07-18 16:11:56 +0200

    unuseful code removed

--- WPF_Primszam/MainWindow.xaml.cs
+++ WPF_Primszam/MainWindow.xaml.cs
@@ -25,12 +25,10 @@ namespace WPF_Primszam
             InitializeComponent();
         }
 
-
         public int Size { get; set; }
-        static bool prime(int a)
+        static bool IsPrim(int a)
         {
             if (a % 2 == 0 || a==1) return false;
-            //int h = Convert.ToInt32(Math.Sqrt(a));
             for(int i=3; i<((int)(Math.Sqrt(a))); i+=2)
             {
                 if (a % i == 0) return false;
@@ -46,36 +44,9 @@ namespace WPF_Primszam
             textBox.AppendText(2 +", ");
             for (int i = 1; i < numbers.Length; i++)
             {
-                if (prime(i)) textBox.AppendText(i + ", ");
+                if (IsPrim(i)) textBox.AppendText(i + ", ");
             }
-
-
-            //textBox.AppendText("2, 5, 7, ");
-            //for (int i = 3; i < numbers.Length; i++)
-            //{
-            //    prim = true;
-            //    if (i % 2 == 0) prim = false;
-
-            //    for (int j = 3; j <=Math.Sqrt((double)(i)) && prim==true; j+=2)
-            //    {
-            //        if (i % j == 0) prim = false;
-            //        if (prim == true) textBox.AppendText(numbers[i] + ", "); 
-            //    }
-
-            //}
-
-
-
-            //textBox.AppendText
-            //if (!(numbers[i] % 2 == 0 || numbers[i] % 3 == 0 || numbers[i] % 5 == 0 || numbers[i] % 7 == 0) && numbers[i] !=1)
-            //{
-            //    textBox.AppendText(numbers[i] + ", ");
-            //}
-        }
-        //foreach (int item in numbers)
-        //{
-        //    textBox.AppendText(item + ", ");
-        //}
+        }        
     }
 }
 
